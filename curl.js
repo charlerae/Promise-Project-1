30d95b623e6ddda1399aa2c529183dec10405306
Alyssa Jean <alyssajean@Alyssas-MacBook-Pro.local>
2020-03-30 19:46:13 -0600

    Modified options variable

--- curl.js
+++ curl.js
@@ -1,3 +1,16 @@
 const fetch = require('node-fetch');
 
-console.log(fetch)
+const dash = require('dashdash');
+
+const options = {
+    allowUnknown: true,
+    options: [{
+        names: ['output', 'o'],
+        type:'string',
+        help: 'file in which to store the fetched content'
+    }],
+};
+const parser = dash.createParser(options);
+
+const opts = parser.parse(options);
+console.log('Options are:', opts);
